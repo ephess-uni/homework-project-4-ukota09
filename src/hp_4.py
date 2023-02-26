dee085cda44b8bd311936292350985ac87444dfb
ukota09 <123261012+ukota09@users.noreply.github.com>
2023-02-25 23:33:47 -0600

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -4,43 +4,41 @@ from datetime import datetime, timedelta
 from csv import DictReader, DictWriter
 from collections import defaultdict
 
-
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    modified_date_list=[]
-    for dat in old_dates:
-        modified_date_list.append(datetime.strptime(dat, "%Y-%m-%d").strftime("%d %b %Y"))
-    return modified_date_list
+    new_dates = list()
+    formatstr = '%Y-%m-%d'
+    for i in old_dates:
+        start = datetime.strptime(i, formatstr)
+        start = start.strftime('%d %b %Y')
+        new_dates.append(str(start))
+    return new_dates
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    
     if not isinstance(start, str):
-        raise TypeError
-    elif not isinstance(n, int):
-        raise TypeError
-    else:
-        added_list=[]
-        for inc in range(0,n):
-            added_list.append(datetime.strptime(start,"%Y-%m-%d")  + timedelta(days=inc))
-        return added_list
-
+        raise TypeError("Only string is valid input for start")
+    if not isinstance(n, int):
+        raise TypeError("Only int is valid input for n")
+    added_dates = list()
+    formatstr = "%Y-%m-%d"
+    start = datetime.strptime(start, formatstr)
+    for i in range(n):
+        added_dates.append(start)
+        start = start + timedelta(days=1)
+    return added_dates
 
 def add_date_range(values, start_date):
-    """Adds a daily date range to the list `values` beginning with 
+    """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    added_list=[]
-    for i, elem in enumerate(values):
-        dat_list=[]       
-        dat_list.append(datetime.strptime(start_date,"%Y-%m-%d")  + timedelta(days=i))
-        dat_list.append(elem)
-        added_list.append(tuple(dat_list))
-    return added_list
-
+    dates = date_range(start_date, len(values))
+    con_dates = tuple(dates)
+    con_values = tuple(values)
+    return list(zip(con_dates, con_values))
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
@@ -81,7 +79,6 @@ def fees_report(infile, outfile):
         writer.writeheader()
         writer.writerows(updated_list)
 
-
 # The following main selection block will only run when you choose
 # "Run -> Module" in IDLE.  Use this section to run test code.  The
 # template code below tests the fees_report function.
@@ -89,8 +86,8 @@ def fees_report(infile, outfile):
 # Use the get_data_file_path function to get the full path of any file
 # under the data directory.
 
-if _name_ == '_main_':
-    
+if __name__ == '__main__':
+   
     try:
         from src.util import get_data_file_path
     except ImportError:
